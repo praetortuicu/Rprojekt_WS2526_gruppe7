5da5861c8948b96e88b8fd59b99ee0297a295ab7
praetortuicu <teodor.ticu@outlook.com>
2026-02-27 17:30:28 +0100

    Add 3 more tests

--- tests/testthat/test-node.R
+++ tests/testthat/test-node.R
@@ -39,3 +39,21 @@ test_that("Node getters and setters",	{
 			testthat::expect_identical(get_right_child(root),	node2)
 
 })
+
+test_that("Node correctly assigns children left/right by value",	{
+			root	<-	Node(value	=	0,			ROOT	=	TRUE,	left	=	NULL,	right	=	NULL)
+			node1	<-	Node(value	=	NA_real_,	ROOT	=	FALSE,	left	=	NULL,	right	=	NULL,	parent	=	root)
+			node2	<-	Node(value	=	NA_real_,	ROOT	=	FALSE,	left	=	NULL,	right	=	NULL,	parent	=	root)
+			node3	<-	Node(value	=	NA_real_,	ROOT	=	FALSE,	left	=	NULL,	right	=	NULL)
+			node4	<-	Node(value	=	NA_real_,	ROOT	=	FALSE,	left	=	NULL,	right	=	NULL)
+
+			set_value(node1,	1)
+			set_value(node2,	2)
+			set_value(node3,	3)
+			set_value(node4,	4)
+
+			testthat::expect_no_error(assign_children(root,	node1,	node2))
+			testthat::expect_less_than(get_value(get_left_child(root)),	get_value(get_right_child(root)))
+			testthat::expect_error(assign_children(root,	node3,	node4))
+
+})
