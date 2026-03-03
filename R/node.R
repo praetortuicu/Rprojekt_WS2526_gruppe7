86ee453c00b15d407970ca09195d570b158fa223
praetortuicu <teodor.ticu@outlook.com>
2026-03-03 16:29:12 +0100

    Optimize and refactor Node class, now uses references

--- R/node.R
+++ R/node.R
@@ -1,54 +1,34 @@
 Node	<-	S7::new_class("Node",
 			properties = list(
-				value		=	S7::new_property(class	=	S7::class_double,	default	=	0.0),
 				ROOT		=	S7::new_property(class	=	S7::class_logical,	default = FALSE),
-				parent_node	=	S7::new_property(								default	=	NULL),
-				left_node	=	S7::new_property(								default	=	NULL),
-				right_node	=	S7::new_property(								default	=	NULL),
-				ref			=	S7::new_property(S7::class_environment,	default	=	new.env(parent	=	emptyenv())),
-				s_feature	=	S7::new_property(S7::class_double,	default	=	NA_real_),
-				s_value		=	S7::new_property(S7::class_double,	default	=	NA_real_),
-				leaf_value	=	S7::new_property(S7::class_double,	default	=	NA_real_)
-			)
+				ref			=	S7::new_property(S7::class_environment,	default	=	new.env(parent	=	emptyenv()))
+			),
+			constructor	=	function(ROOT=FALSE,	s_feature=NA_real_,	s_value=NA_real_,	leaf_value=NA_real_)	{
+				env	<-	new.env(parent	=	emptyenv())
+				env$s_feature	<-	s_feature
+				env$s_value		<-	s_value
+				env$leaf_value	<-	leaf_value
+				env$left_node	<-	NULL
+				env$right_node	<-	NULL
+				S7::new_object(S7::S7_object(),	ROOT=ROOT,	ref=env)
+			}
 )
 
 ###		GENERICS	###
 is_root				<-	S7::new_generic("is_root",	"node")
 is_leaf				<-	S7::new_generic("is_leaf",	"node")
 
-get_parent			<-	S7::new_generic("get_parent",	"node")
-set_parent			<-	S7::new_generic("set_parent",	"node")
 get_value			<-	S7::new_generic("get_value",	"node")
 set_value			<-	S7::new_generic("set_value",	"node")
 
-get_left_child		<-	S7:::new_generic("get_left_child",	"node")
-get_right_child		<-	S7:::new_generic("get_right_child",	"node")
-set_left_child		<-	S7:::new_generic("get_left_child",	"node")
-set_right_child		<-	S7:::new_generic("set_right_child",	"node")
-assign_children		<-	S7::new_generic("assign_children",	"node")
-
-assign_left_child	<-	S7::new_generic("assign_left_child",	"node")
-assign_right_child	<-	S7::new_generic("assign_right_child",	"node")
-remove_child		<-	S7::new_generic("remove_child",			"node")
+get_left_child		<-	S7::new_generic("get_left_child",	"node")
+get_right_child		<-	S7::new_generic("get_right_child",	"node")
+set_left_child		<-	S7::new_generic("get_left_child",	"node")
+set_right_child		<-	S7::new_generic("set_right_child",	"node")
 
 ###		GETTERS	###
 
-#'	Get parent node of target node
-#'
-#'	@name	Node@get_parent
-#'	@param	node				A node object
-#'	@return	node@parent_node	A node object
-#'	@examples
-#'	get_parent(node)
-S7::method(get_parent,	Node)	<-	function(node)	{
-			if	(is_root(node))	{
-				stop("Root node does not have a parent!\n")
-			}	else	{
-				return(node@ref$parent_node)
-			}
-}
-
-#'	Get value of target node
+#'	Get value of target node. If the node is a leaf, then leaf_value is returned, otherwise the split value is returned.
 #'
 #'	@name	Node@get_value
 #'	@param	node				A node object
@@ -56,7 +36,8 @@ S7::method(get_parent,	Node)	<-	function(node)	{
 #'	@examples
 #'	get_value(node)
 S7::method(get_value,	Node)	<-	function(node)	{
-			return(node@value)
+			if	(is_leaf(node))	{	return(node@ref$leaf_value)	}
+			return(node@ref$s_value)
 }
 
 #'	Get the left child of the node
@@ -67,7 +48,10 @@ S7::method(get_value,	Node)	<-	function(node)	{
 #'	@examples
 #'	get_left_child(node)
 S7::method(get_left_child,	Node)	<-	function(node)	{
+			if	(!is_leaf(node))	{
 			return(node@ref$left_node)
+			}
+			stop("Leaves do not have child nodes!\n")
 }
 
 #'	Get the right child of the node
@@ -78,25 +62,13 @@ S7::method(get_left_child,	Node)	<-	function(node)	{
 #'	@examples
 #'	get_right_child(node)
 S7::method(get_right_child,	Node)	<-	function(node)	{
+			if	(!is_leaf(node))	{
 			return(node@ref$right_node)
+			}
+			stop("Leaves do not have child nodes!\n")
 }
 
 ###		SETTERS	###
-
-#'	Set the parent node of the target to a new node object
-
-#'	@name	Node@set_parent
-#'	@param	node			Target node object
-#'	@param	new_parent_node	New node object
-#'	@examples
-#'	set_parent(target,	new_parent)
-#'	set_parent(root,	new_parent)
-S7::method(set_parent,	Node)	<-	function(node,	new_parent_node	)	{
-			if	(is_root(node))		{	stop("Root node does not have a parent!\n")	}
-			#paste0("Setting parent node of ", node, " ...\n")
-
-			node@ref$parent_node	<-	new_parent_node
-}
 
 #'	Set value of target node
 #'
@@ -107,7 +79,11 @@ S7::method(set_parent,	Node)	<-	function(node,	new_parent_node	)	{
 #'	@examples
 #'	set_value(node)
 S7::method(set_value,	Node)	<-	function(node,	value)	{
-			node@ref$value	<-	value
+			if	(!is_leaf(node))	{
+			node@ref$s_value	<-	value
+			}	else	{
+			node@ref$leaf_value	<-	value
+			}
 }
 
 #'	Set the left child of the node
@@ -144,81 +120,26 @@ S7::method(set_right_child,	Node)	<-	function(node,	new_node)	{
 #'	@return	True/False
 #'	@examples
 #'	is_root(node)
-
 S7::method(is_root,	Node)	<-	function(node)	{
 			if	(node@ROOT)	{
 				return(TRUE)
 			}	else	{	return(FALSE)	}
 }
 
+#'	Verify if node is a leaf
+#'
+#'	@name	Node@is_leaf
+#'	@param	node	A node object
+#'	@return	True/False
+#'	@examples
+#'	is_leaf(node)
+S7::method(is_leaf,	Node)	<-	function(node)	{
+			if	(node@ROOT)	{	return(FALSE)	}
+			else	{
+				return(is.null(node@ref$left_node)	&&	is.null(node@ref$right_node))
+			}
+}
+
+
 ###		DATA	###
 
-#'	Assign left child to a node
-#'	@name	node@assign_left_child
-#'	@param	node	A node object
-#'	@param	child2be	A node object
-#'	@return	void
-#'	@examples
-#'	assign_left_child(node,	child)
-
-S7::method(assign_left_child,	Node)	<-	function(node,	child2be)	{
-			if	(is.null(child2be))	{
-				stop("Cannot add invalid object as child!\n")
-			}	else	if	(identical(get_left_child(node),	NULL))	{
-				set_left_child(node,	child2be)
-			}	else	if	(get_value(get_left_child(node))	>	get_value(child2be))	{
-				#	TODO
-			}	else	if	(get_value(get_left_child(node))	<	get_value(child2be))	{
-				#	TODO
-			}
-}
-
-#'	Assign right child to a node
-#'	@name	node@assign_right_child
-#'	@param	node	A node object
-#'	@param	child2be	A node object
-#'	@return	void
-#'	@examples
-#'	assign_right_child(node,	child)
-
-S7::method(assign_right_child,	Node)	<-	function(node,	child2be)	{
-			if	(is.null(child2be))	{
-				stop("Cannot add invalid object as child!\n")
-			}	else	if	(identical(get_right_child(node),	NULL))	{
-				set_right_child(node,	child2be)
-			}	else	if	(get_value(get_right_child(node))	>	get_value(child2be))	{
-				#	TODO
-			}	else	if	(get_value(get_left_child(node))	<	get_value(child2be))	{
-				#	TODO
-			}
-}
-#	TODO refactor this to be less text see function below
-#'	Assign children to a node
-#'	@name	node@assign_children
-#'	@param	node	A node object, to which children are assigned
-#'	@param	child1	A node object, to assign to node
-#'	@param	child2	(Optional) A further node object, to assign to node
-#'	@return	void
-#'	@examples
-#'	assign_children(node,	child1,	child2)
-#'	assign_children(node,	child)
-
-S7::method(assign_children,	Node)	<-	function(node,	child1,	child2=NULL)	{
-			if	(is.null(child1))	{
-				stop("You must provide at least one node!\n")
-			}	else	if	(get_value(get_right_child(node))	>	get_value(child1))	{
-				assign_left_child(node,	child1)
-			}
-}
-
-#'	Remove child from node
-#'	@name	node@remove_child
-#'	@param	node	A (parent) node object
-#'	@param	node	A (child) node object
-#'	@param	return	void
-#'	@examples
-#'	remove_child(node,	child)
-
-S7::method(remove_child,	Node)	<-	function(node,	child)	{
-			#	TODO
-}
--- tests/testthat/test-node.R
+++ tests/testthat/test-node.R
@@ -1,67 +1,53 @@
 test_that("Node initialization",	{
-			root	<-	Node(value	=	0,	ROOT	=	TRUE,	left	=	NULL,	right	=	NULL)
+			root	<-	Node(s_value	=	0,	ROOT	=	TRUE)
 			testthat::expect_s7_class(root, Node)
 
-			node	<-	Node(value	=	1,	ROOT	=	FALSE,	left	=	NULL,	right	=	NULL,	parent	=	root)
+			node	<-	Node(s_value	=	1,	ROOT	=	FALSE)
 			testthat::expect_s7_class(node,	Node)
-})
 
-test_that("Node	validation",	{
-			root	<-	Node(value	=	0,ROOT	=	TRUE,	left	=	NULL,	right	=	NULL)
-			node	<-	Node(value	=	1,ROOT	=	FALSE,	left	=	NULL,	right	=	NULL,	parent	=	root)
-			testthat::expect_true(is_root(root),	TRUE)
-			testthat::expect_false(is_root(node),	FALSE)
+			testthat::expect_true(is_root(root))
+			testthat::expect_false(is_root(node))
+
+			testthat::expect_false(is_leaf(root))
+			testthat::expect_true(is_leaf(node))
 })
 
 test_that("Node getters and setters",	{
-			root	<-	Node(value	=	0,			ROOT	=	TRUE,	left	=	NULL,	right	=	NULL)
-			node	<-	Node(value	=	NA_real_,	ROOT	=	FALSE,	left	=	NULL,	right	=	NULL,	parent	=	root)
-			node2	<-	Node(value	=	NA_real_,	ROOT	=	FALSE,	left	=	NULL,	right	=	NULL,	parent	=	root)
+			root	<-	Node(s_value	=	0,							ROOT	=	TRUE)
+			node	<-	Node(s_value	=	1,	leaf_value	=	0.5,	ROOT	=	FALSE)
+			node1	<-	Node(s_value	=	2,	leaf_value	=	1.5,	ROOT	=	FALSE)
 
-			testthat::expect_no_error(set_parent(node,	root))
-			testthat::expect_no_error(set_parent(node2,	root))
+			set_left_child(root,	node)
+			set_right_child(root,	node1)
 
-			testthat::expect_error(get_parent(root))
-			testthat::expect_error(set_parent(root,	node))
+			testthat::expect_true(is_leaf(node))
+			testthat::expect_true(is_leaf(node1))
 
-			testthat::expect_identical(get_parent(node),	root)
+			testthat::expect_true(identical(get_left_child(root),	node))
+			testthat::expect_true(identical(get_right_child(root),	node1))
 
-			testthat::expect_identical(get_value(node),		NA_real_)
-			testthat::expect_identical(get_value(node2),	NA_real_)
+			testthat::expect_equal(get_value(root),		0)
+			testthat::expect_false(identical(get_value(node),	1))
+			testthat::expect_false(identical(get_value(node1),	2))
 
-			testthat::expect_identical(set_value(node,	1),		1)
-			testthat::expect_identical(set_value(node,	2),		2)
+			#	These nodes are leaves as of now
+			testthat::expect_true(identical(get_value(node),	0.5))
+			testthat::expect_true(identical(get_value(node1),	1.5))
 
-			testthat::expect_no_error(set_left_child(root,	node))
-			testthat::expect_no_error(set_right_child(root,	node2))
-			
-			testthat::expect_identical(get_left_child(root),	node)
-			testthat::expect_identical(get_right_child(root),	node2)
+			node2	<-	Node(s_value	=	3,	leaf_value	=	7,		ROOT	=	FALSE)
+			node3	<-	Node(s_value	=	4,	leaf_value	=	3.5,	ROOT	=	FALSE)
 
-})
+			set_left_child(node,	node2)
+			set_right_child(node,	node3)
 
-test_that("Node correctly assigns children left/right by value",	{
-			root	<-	Node(value	=	0,			ROOT	=	TRUE,	left	=	NULL,	right	=	NULL)
-			node1	<-	Node(value	=	NA_real_,	ROOT	=	FALSE,	left	=	NULL,	right	=	NULL,	parent	=	root)
-			node2	<-	Node(value	=	NA_real_,	ROOT	=	FALSE,	left	=	NULL,	right	=	NULL,	parent	=	root)
-			node3	<-	Node(value	=	NA_real_,	ROOT	=	FALSE,	left	=	NULL,	right	=	NULL)
-			node4	<-	Node(value	=	NA_real_,	ROOT	=	FALSE,	left	=	NULL,	right	=	NULL)
+			testthat::expect_identical(get_left_child(node),	node2)
+			testthat::expect_identical(get_right_child(node),	node3)
 
-			set_value(node1,	1)
-			set_value(node2,	2)
-			set_value(node3,	3)
-			set_value(node4,	4)
 
-			testthat::expect_error(assign_left_child(root,	NULL))
-			testthat::expect_error(assign_right_child(root,	NULL))
-			testthat::expect_no_error(assign_left_child(root,	node1))
-			testthat::expect_error(assign_left_child(root,	node3))
-			testthat::expect_no_error(assign_right_child(root,	node2))
-			testthat::expect_no_error(remove_child(root,	node1))
-			testthat::expect_no_error(remove_child(root,	node2))
+			testthat::expect_false(is_leaf(node))
+			testthat::expect_true(is_leaf(node1))
 
-			testthat::expect_no_error(assign_children(root,	node1,	node2))
-			testthat::expect_lt(get_value(get_left_child(root)),	get_value(get_right_child(root)))
-			testthat::expect_error(assign_children(root,	node3,	node4))
-
+			# node is not a leaf anymore, node1 is
+			testthat::expect_true(identical(get_value(node),	1))
+			testthat::expect_false(identical(get_value(node1),	2))
 })
