1a36c28d16468aa4488f829214c1ac51291ca7e1
Paul P <paul.proft@stud.uni-heidelberg.de>
2026-03-03 15:58:51 +0100

    tested functions, buttons and front/back end spilt

--- R/test_func.R
+++ R/test_func.R
@@ -1,2 +1,19 @@
-c("Hello World!")
-print(c)
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("Hello World!"),
+  textInput("c", "Enter Text:"),
+  actionButton("clickme", "Click Me!"),
+  textOutput("result")
+
+)
+
+server <- function(input, output){
+  output$c <- renderText("Hello World!")
+  observeEvent(input$clickme, {
+    output$result <- renderText(paste("Congrats you learned inputs dear ", input$c))
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
