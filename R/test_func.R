3a08539b7a1b17a5a48fded9b1a32979fd6a0aa9
Paul P <paul.proft@stud.uni-heidelberg.de>
2026-03-04 01:03:46 +0100

    New To-Do List and experimented further with shiny

--- R/test_func.R
+++ R/test_func.R
@@ -1,17 +1,36 @@
 library(shiny)
 
-ui <- fluidPage(
-  titlePanel("Hello World!"),
-  textInput("c", "Enter Text:"),
-  actionButton("clickme", "Click Me!"),
-  textOutput("result")
+m_offset <- c(0, 1.5)
+r_offset <- c(0.5, 1.5)
+l_offset <- c(-0.5, 1.5)
 
+lin_offset <- c(0, 0)
+
+ui <- fluidPage(
+  plotOutput("plot")
 )
 
-server <- function(input, output){
-  output$c <- renderText("Hello World!")
-  observeEvent(input$clickme, {
-    output$result <- renderText(paste("Congrats you learned inputs dear ", input$c))
+
+server <- function(input, output) {
+  output$plot <- renderPlot({
+    plot(0, 0,
+         type = "n",      # nichts zeichnen
+         xlim = c(-10, 10),
+         ylim = c(-10, 10),
+         xlab = "X",
+         ylab = "Y")
+    
+    points(0, 0, col = "red", pch = 19, cex = 2, main = "Root")
+    points(-2, -4, col = "blue", pch = 19, cex = 2, main = "Child L")
+    points(2, -4, col = "blue", pch = 19, cex = 2, main = "Child R")
+    segments(0, 0, -2, -4, col = "blue", lwd = 2, main = "condition 1")
+    segments(0, 0, 2, -4, col = "blue", lwd = 2, main = "condition 2")
+    
+    text(col = "red", x = 0, y = 0 + m_offset[2], labels = "Root", cex = 1.5)
+    text(col = "blue", x = -2 + l_offset[1], y = -4 + l_offset[2] , labels = "Child L", cex = 1.5)
+    text(col = "blue", x = 2 + r_offset[1], y = -4 + r_offset[2] , labels = "Child R", cex = 1.5)
+    text(col = "black", x = -1 + lin_offset[1], y = -2 + lin_offset[2] , labels = "condition 1", cex = 1)
+    text(col = "black", x = 1 + lin_offset[1], y = -2 + lin_offset[2] , labels = "condition 2", cex = 1)
   })
 }
 
