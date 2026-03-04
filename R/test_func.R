8c258e1ba43b1702259035357443b721ff01d3f1
Paul P <paul.proft@stud.uni-heidelberg.de>
2026-03-04 13:19:45 +0100

    Parallel Ui elements with absolute positions tested

--- R/test_func.R
+++ R/test_func.R
@@ -1,57 +1,73 @@
 library(shiny)
 
 ui <- fluidPage(
-  plotOutput(
-    "plot",
-    click = "plot_click",
-    hover = hoverOpts("plot_hover", delay = 0)
-  ),
-  verbatimTextOutput("info")
+  div(
+    style = "position: relative; height: 600px;",
+    
+    div(
+      style = "
+        position: absolute;
+        top: 50px;
+        left: 100px;
+        width: 300px;
+        height: 300px;
+      ",
+      plotOutput("plot1", height = "100%", click = "click1")
+    ),
+    
+    div(
+      style = "
+        position: absolute;
+        top: 200px;
+        left: 450px;
+        width: 400px;
+        height: 200px;
+      ",
+      plotOutput("plot2", height = "100%", click = "click2")
+    )
+  )
 )
 
 server <- function(input, output, session) {
   
-  point <- reactiveVal(c(5, 5))
-  dragging <- reactiveVal(FALSE)
+  # Separate Punkt-Speicher
+  points1 <- reactiveVal(matrix(ncol = 2, nrow = 0))
+  points2 <- reactiveVal(matrix(ncol = 2, nrow = 0))
   
-  # Start / Stop dragging when clicking near the point
-  observeEvent(input$plot_click, {
-    
-    p <- point()
-    d <- sqrt((input$plot_click$x - p[1])^2 +
-                (input$plot_click$y - p[2])^2)
-    
-    if (d < 0.5) {
-      dragging(!dragging())   # toggle dragging
-    }
+  # Klick für Canvas 1
+  observeEvent(input$click1, {
+    new_point <- c(input$click1$x, input$click1$y)
+    points1(rbind(points1(), new_point))
   })
   
-  # Move point while dragging
-  observe({
-    if (dragging() && !is.null(input$plot_hover)) {
-      point(c(input$plot_hover$x, input$plot_hover$y))
-    }
+  # Klick für Canvas 2
+  observeEvent(input$click2, {
+    new_point <- c(input$click2$x, input$click2$y)
+    points2(rbind(points2(), new_point))
   })
   
-  output$plot <- renderPlot({
-    plot(0, 0,
-         type = "n",
+  output$plot1 <- renderPlot({
+    plot(0, 0, type = "n",
          xlim = c(0, 10),
-         ylim = c(0, 10))
+         ylim = c(0, 10),
+         main = "Canvas 1")
     
-    p <- point()
-    
-    points(p[1], p[2],
-           pch = 19,
-           col = if (dragging()) "blue" else "red",
-           cex = 2)
+    if (nrow(points1()) > 0) {
+      points(points1()[,1], points1()[,2],
+             pch = 19, col = "red")
+    }
   })
   
-  output$info <- renderPrint({
-    list(
-      position = point(),
-      dragging = dragging()
-    )
+  output$plot2 <- renderPlot({
+    plot(0, 0, type = "n",
+         xlim = c(0, 10),
+         ylim = c(10, 100),
+         main = "Canvas 2")
+    
+    if (nrow(points2()) > 0) {
+      points(points2()[,1], points2()[,2],
+             pch = 19, col = "blue")
+    }
   })
 }
 
