234295c77b87c15b2f466ed146349de9c3cb033a
Paul P <paul.proft@stud.uni-heidelberg.de>
2026-03-05 12:08:32 +0100

    Data-visualise Lösung gefunden

--- R/test_func.R
+++ R/test_func.R
@@ -1,73 +1,27 @@
 library(shiny)
 
 ui <- fluidPage(
+  
+  h3("Datenbank-Fenster"),
+  
   div(
-    style = "position: relative; height: 600px;",
+    style = "
+      border: 2px solid black;
+      height: 250px;
+      width: 400px;
+      overflow-y: scroll;
+      padding: 10px;
+      background-color: #f9f9f9;
+    ",
     
-    div(
-      style = "
-        position: absolute;
-        top: 50px;
-        left: 100px;
-        width: 300px;
-        height: 300px;
-      ",
-      plotOutput("plot1", height = "100%", click = "click1")
-    ),
-    
-    div(
-      style = "
-        position: absolute;
-        top: 200px;
-        left: 450px;
-        width: 400px;
-        height: 200px;
-      ",
-      plotOutput("plot2", height = "100%", click = "click2")
-    )
+    tableOutput("db_table")
   )
 )
 
-server <- function(input, output, session) {
+server <- function(input, output) {
   
-  # Separate Punkt-Speicher
-  points1 <- reactiveVal(matrix(ncol = 2, nrow = 0))
-  points2 <- reactiveVal(matrix(ncol = 2, nrow = 0))
-  
-  # Klick für Canvas 1
-  observeEvent(input$click1, {
-    new_point <- c(input$click1$x, input$click1$y)
-    points1(rbind(points1(), new_point))
-  })
-  
-  # Klick für Canvas 2
-  observeEvent(input$click2, {
-    new_point <- c(input$click2$x, input$click2$y)
-    points2(rbind(points2(), new_point))
-  })
-  
-  output$plot1 <- renderPlot({
-    plot(0, 0, type = "n",
-         xlim = c(0, 10),
-         ylim = c(0, 10),
-         main = "Canvas 1")
-    
-    if (nrow(points1()) > 0) {
-      points(points1()[,1], points1()[,2],
-             pch = 19, col = "red")
-    }
-  })
-  
-  output$plot2 <- renderPlot({
-    plot(0, 0, type = "n",
-         xlim = c(0, 10),
-         ylim = c(10, 100),
-         main = "Canvas 2")
-    
-    if (nrow(points2()) > 0) {
-      points(points2()[,1], points2()[,2],
-             pch = 19, col = "blue")
-    }
+  output$db_table <- renderTable({
+    mtcars
   })
 }
 
