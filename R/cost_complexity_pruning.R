a7abe5ec3750cfe2098153f347fe3358b69193a9
Yousef Khell <yousef@khell.com>
2026-03-13 10:04:14 +0100

    Update skeleton to support Theo's final CART implementation

--- R/cost_complexity_pruning.R
+++ R/cost_complexity_pruning.R
@@ -1,71 +1,60 @@
 # cost_complexity_pruning.R
 
-# Error in subtree with root at given node in tree
-get_subtree_error <- S7::new_generic("get_subtree_error", "tree")
-# Find the weakest link vgl. Satz 6.19
-get_weakest_link <- S7::new_generic("get_weakest_link", "tree")
-# Collapse the subtree to a single leaf
-prune_node <- S7::new_generic("prune_node", "tree")
-# The whole shabang
+###     GENERICS      ###
+get_subtree_error     <- S7::new_generic("get_subtree_error",     "tree")
+get_weakest_link      <- S7::new_generic("get_weakest_link",      "tree")
+prune_node            <- S7::new_generic("prune_node",            "tree")
 cost_complexity_prune <- S7::new_generic("cost_complexity_prune", "tree")
 
-# Data: X matrix, y vector
 
-###   METHODS   ###
+###     HELPERS     ###
 
-#' Compute the error of a subtree with root at a given node
-#'
-#' @name BinaryTree@get_subtree_error
-#' @param tree A BinaryTree object
-#' @param node A node object - the root of the subtree
-#' @param y Numeric vector
-#' @param X Numeric Matrix
-#' @return Numeric
-#' @export
+# Count leaves in subtree rooted at given node
+count_leaves <- function(node) {
+  # TODO
+}
+
+# Compute the error R(t) if subtree was replaced by single leaf
+compute_node_error <- function(node, root, X, y) {
+  # TODO
+}
+
+# Compute the subtree error R(T_t)
+sum_leaf_errors <- function(node, root, X, y) {
+  # TODO
+}
+
+# Compute the cost complexity ratio (R(t) - R(T_t))/(#T_t - 1) for a node, where
+# - R(t) error if the subtree rooted at t gets replaced by leaf (make function for this)
+# - R(T_t) total error in the node's subtree (make function for this)
+# - #T_t number of leaves in the node's subtree (make function for this)
+compute_cost_complexity_ratio <- function(node, root, X, y) {
+  # TODO 
+}
+
+
+###     METHODS     ###
+
+# Compute total resubstitution error of a subtree
+#
+# Wrap sum_leaf_errors() as an S7 method on BinaryTree
 S7::method(get_subtree_error, BinaryTree) <- function(tree, node, X, y) {
   # TODO
 }
 
-#' Find the weakest link in the tree
-#'
-#' Computes the cost-complexity factor (alpha)
-#' for every internal node and returns the one with
-#' the lowest value. vgl. Satz 6.19 but umformuliert.
-#'
-#' @name BinaryTree@get_weakest_link
-#' @param tree  A BinaryTree object
-#' @param X Matrix of predictor values
-#' @param y Numeric vector of true response values
-#' @return A Node object corresponding to the weakest link
-#' @export
+# Find the weakest link in the tree like in the Satz 6.19
+# 
+# For each internal node t we want to compute the cost-complextiy ratio
+# and find the one that minimizes it
 S7::method(get_weakest_link, BinaryTree) <- function(tree, X, y) {
   # TODO
 }
 
-# THIS HAS BEEN ADDED TO THEO'S CODEBASE
-#' Prune a node by collapsing its subtree into a leaf
-#'
-#' @name BinaryTree@prune_node
-#' @param tree  A BinaryTree object
-#' @param node  A Node object to collapse
-#' @param y Numeric vector of true response values for node's region
-#' @return void
-#' @export
-S7::method(prune_node, BinaryTree) <- function(tree, node, y) {
-  # TODO
-}
-
-#' Cost complexity pruning
-#'
-#' Constructs the full sequence T(0), T(1), ..., T(P) from Satz 6.19.
-#' Returns all candidate trees with their associated alpha values.
-#'
-#' @name BinaryTree@cost_complexity_prune
-#' @param tree  A BinaryTree object (fully grown)
-#' @param X Matrix of predictor values
-#' @param y Numeric vector of true response values
-#' @return A list with elements: (trees, alphas)
-#' @export
+# Cost complexity pruning: the whole shabang
+# TODO: 
+#
+# Start with full tree, iteratively prune the weakest link until we have just the root.
+# Important: we need to return the sequence WITH the ratios, so we need to store them as we go.
 S7::method(cost_complexity_prune, BinaryTree) <- function(tree, X, y) {
   # TODO
 }
